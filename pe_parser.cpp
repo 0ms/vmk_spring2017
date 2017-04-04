0f7ed18b8e4944718e03288f0f03065d7d9a639a
Iurii Zykov <jury.zykov@yandex.ru>
2017-04-05 00:52:25 +0300

    initial commit

--- pe_parser.cpp
+++ pe_parser.cpp
@@ -1,6 +1,11 @@
 #include <Windows.h>
 #include <stdio.h>
 
+#define NON_EXECUTABLE_IMAGE "Non-executable image"
+#define NT_SIGNATURE_NOT_FOUND "NT_SIGNATURE not found"
+#define DOS_SIGNATURE_NOT_FOUND "DOS_SIGNATURE not found"
+#define BUFFER_OVERFLOW "Buffer overflow"
+
 #define BUFFER_SIZE 0x1000
 #define CYRILLIC_CODE_PAGE 1251 
 
@@ -8,6 +13,7 @@ HANDLE GetFileFromArguments( int argc, char** argv );
 unsigned int ReadFileToBuffer( HANDLE fileHandle, char buffer[ BUFFER_SIZE ] );
 void PrintHelp( char* programName );
 void PrintError( char* functionFrom );
+void PrintErrorAdv(const char* functionFrom, const char* error);
 void ParseFile( char* buffer, int bufferSize );
 
 int main( int argc, char** argv )
@@ -96,7 +102,67 @@ void ParseFile( char* buffer, int bufferSize )
   //IMAGE_SECTION_HEADER заголовок секции, в нем содержитс€ название, размер и расположение секции
   //
   // Ќе забывайте провер€ть такие пол€ как сигнатуры файлов (ведь надо убедитьс€, что разбираем собственно исполн€емый файл)
-  printf( "Buffer length: %d\nImplement parsing of file\n", bufferSize );
+  //printf( "Buffer length: %d\nImplement parsing of file\n", bufferSize );
+  IMAGE_DOS_HEADER* dos_header = (IMAGE_DOS_HEADER*)buffer;
+  if (dos_header->e_magic == IMAGE_DOS_SIGNATURE) {
+    IMAGE_NT_HEADERS* pe_header
+      = (IMAGE_NT_HEADERS*)(buffer + dos_header->e_lfanew);
+    if (pe_header->Signature == IMAGE_NT_SIGNATURE) {
+      IMAGE_FILE_HEADER* file_header = &pe_header->FileHeader;
+      WORD numberOfSections = file_header->NumberOfSections;
+      DWORD imageBase, entryPoint, sectionOffset;
+      if (pe_header->OptionalHeader.Magic == IMAGE_NT_OPTIONAL_HDR32_MAGIC) {
+        IMAGE_NT_HEADERS32* pe_header
+          = (IMAGE_NT_HEADERS32*)(buffer + dos_header->e_lfanew);
+        IMAGE_OPTIONAL_HEADER32* opt_header = &pe_header->OptionalHeader;
+        sectionOffset = dos_header->e_lfanew + sizeof(*pe_header);
+        imageBase = opt_header->ImageBase;
+        entryPoint = opt_header->AddressOfEntryPoint;
+      } else if (pe_header->OptionalHeader.Magic
+        == IMAGE_NT_OPTIONAL_HDR64_MAGIC) {
+        IMAGE_NT_HEADERS64* pe_header
+          = (IMAGE_NT_HEADERS64*)(buffer + dos_header->e_lfanew);
+        IMAGE_OPTIONAL_HEADER64* opt_header = &pe_header->OptionalHeader;
+        sectionOffset = dos_header->e_lfanew + sizeof(*pe_header);
+        imageBase = opt_header->ImageBase;
+        entryPoint = opt_header->AddressOfEntryPoint;
+      }
+      if (pe_header->OptionalHeader.Magic != IMAGE_ROM_OPTIONAL_HDR_MAGIC) {
+        int i;
+        char* sectionsBase = buffer + sectionOffset;
+        for (i = 0; i < numberOfSections; ++i) {
+          IMAGE_SECTION_HEADER* sec_header
+            = (IMAGE_SECTION_HEADER*)(sectionsBase + i * sizeof(*sec_header));
+          if (sectionOffset + (i + 1) * sizeof(*sec_header) > bufferSize) {
+            PrintErrorAdv(__func__, BUFFER_OVERFLOW);
+            break;
+          }
+          DWORD sectionRVA = sec_header->VirtualAddress;
+          DWORD sectionSize
+            = min(sec_header->SizeOfRawData, sec_header->Misc.VirtualSize);
+          DWORD sectionSizeTmp = sec_header->Misc.VirtualSize;
+          sec_header->Misc.VirtualSize = 0;
+          if (sectionRVA <= entryPoint
+            && entryPoint < sectionRVA + sectionSize) {
+            printf("Entry point (0x%X)\nIn section %d, %s\n\
+Offset in section 0x%X, %d %%\n", imageBase + entryPoint, i, sec_header->Name,
+              entryPoint - sectionRVA,
+              ((entryPoint - sectionRVA) * 100) / sectionSize);
+            sec_header->Misc.VirtualSize = sectionSizeTmp;
+            break;
+          }
+          sec_header->Misc.VirtualSize = sectionSizeTmp;
+        }
+      } else {
+        PrintErrorAdv(__func__, NON_EXECUTABLE_IMAGE);
+      }
+    } else {
+      PrintErrorAdv(__func__, NT_SIGNATURE_NOT_FOUND);
+    }
+  } else {
+    PrintErrorAdv(__func__, DOS_SIGNATURE_NOT_FOUND);
+  }
+  return;
 }
 
 #pragma region __ Print functions __
@@ -120,6 +186,11 @@ void PrintError( char* functionFrom )
 
   printf( "In function %s, error %d:\n%s", functionFrom, errorCode, errorMessage );
   LocalFree( errorMessage );
+}
+
+void PrintErrorAdv(const char* functionFrom, const char* error) {
+  printf("%s: %s\n", functionFrom, error);
+  return;
 }
 
 #pragma endregion
